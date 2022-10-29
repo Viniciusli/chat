4385cde884bdfd875ac8131bd25fbd0619ac5c9a
Vinicius Lee <Viniciusleeg@outlook.com>
2022-10-29 13:56:08 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'public'))
+app.use(express.static(path.join(__dirname, '../public')))
+app.set('views', path.join(__dirname, '../public'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
